d8f204dc8c7941f779a3390fd13ec4489c73fce4
Shahwaz Khan <sshaizkhan@gmail.com>
2021-04-17 16:32:41 -0700

    create catkin workspace and add ros package

---
+++ build/pick_n_place/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shahwaz/Documents/dev-shahwaz-khan/src/pick_n_place
+# Build directory: /home/shahwaz/Documents/dev-shahwaz-khan/build/pick_n_place
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
